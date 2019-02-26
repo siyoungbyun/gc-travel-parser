0bd9d2c608e313ae9e599eec45042c04a2f03655
Colin Quirk <cquirk@uchicago.edu>
2019-02-26 13:09:20 -0600

    add basic views

--- gctravelparser/views.py
+++ gctravelparser/views.py
@@ -1,6 +1,17 @@
+from flask import render_template
 from gctravelparser import app
 
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.html')
+
+
+@app.route('/basic')
+def basic():
+    return render_template('basic.html')
+
+
+@app.route('/advanced')
+def advanced():
+    return render_template('advanced.html')
