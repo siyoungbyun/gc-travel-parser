ef08b94a7114a798a82fddf4377716e688df13f0
Colin Quirk <cquirk@uchicago.edu>
2019-02-28 10:42:34 -0600

    Add basic class structure

---
+++ gctravelparser/models.py
@@ -0,0 +1,19 @@
+class Applicant:
+    """ A class containing information about the user who submitted the application
+    """
+    def __init__():
+        pass
+
+
+class Reviewer:
+    """ A class containing information about the user who submitted the application
+    """
+    def __init__():
+        pass
+
+
+class Application:
+    """ A class containing information about the application itself
+    """
+    def __init__():
+        pass
