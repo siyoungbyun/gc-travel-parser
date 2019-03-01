0d10177ad60ae6551165ecd10f941bf7a627aa8c
Colin Quirk <cquirk@uchicago.edu>
2019-02-28 20:20:53 -0600

    sample model structure

--- gctravelparser/models.py
+++ gctravelparser/models.py
@@ -1,19 +1,19 @@
-class Applicant:
+from gctravelparser import db
+
+
+class Applicant(db.Model):
     """ A class containing information about the user who submitted the application
     """
-    def __init__():
-        pass
+    uuid = db.Column(db.Integer, primary_key=True)
 
 
-class Reviewer:
+class Reviewer(db.Model):
     """ A class containing information about the user who submitted the application
     """
-    def __init__():
-        pass
+    uuid = db.Column(db.Integer, primary_key=True)
 
 
-class Application:
+class Application(db.Model):
     """ A class containing information about the application itself
     """
-    def __init__():
-        pass
+    uuid = db.Column(db.Integer, primary_key=True)
