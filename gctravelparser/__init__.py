d10c1da50f27797202912f8978e8c31059340c68
Colin Quirk <cquirk@uchicago.edu>
2019-03-07 23:52:58 -0600

    Add version numbers

--- gctravelparser/__init__.py
+++ gctravelparser/__init__.py
@@ -4,6 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['VERSION_NUMBER_MAJOR'] = 0
+app.config['VERSION_NUMBER_MINOR'] = 1
+app.config['VERSION_NUMBER_PATCH'] = 0
 db = SQLAlchemy(app)
 
 import gctravelparser.views  # noqa: F401,E402
